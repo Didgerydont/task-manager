8fa30c899c9b86d24c7fea6e4295be6f2a8d3f60
Ubuntu <ubuntu@ip-172-31-89-142.ec2.internal>
2019-11-24 20:06:25 +0000

    adding navbar

--- app.py
+++ app.py
@@ -78,6 +78,18 @@ def delete_category(category_id):
     mongo.db.categories.remove({'id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
 
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+    
+    
+@app.route('/new_category')
+def new_category():
+    return render_template('addcategory.html')
+    
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
